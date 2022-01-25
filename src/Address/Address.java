e0b50c3a39a2607f681132e59f85c42ec2c9e3e5
Choi han byeol <chbyol@naver.com>
2022-01-25 09:37:03 +0900

    22/1/17 수업기록

---
+++ src/Address/Address.java
@@ -0,0 +1,12 @@
+package Address;
+
+
+public class Address {
+
+	public static void main(String[] args) {
+
+		System.out.println("서울시 서초구 우리집");
+
+	}
+
+}
---
+++ src/Address/Number.java
@@ -0,0 +1,10 @@
+package Address;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println("전화번호 : 010-4739-4223");
+
+	}
+
+}
